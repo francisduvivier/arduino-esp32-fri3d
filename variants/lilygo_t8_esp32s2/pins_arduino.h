ba940d08a272c0a0d26d437f911173fc445ee8b9
Francis Duvivier <francisduvivier@gmail.com>
2024-08-17 10:31:45 +0200

    change RX and TX to 17 and 18

--- variants/lilygo_t8_esp32s2/pins_arduino.h
+++ variants/lilygo_t8_esp32s2/pins_arduino.h
@@ -19,8 +19,8 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+18; // GPIO pin for Saola-
 #define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 46)
 
-static const uint8_t TX = 43;
-static const uint8_t RX = 44;
+static const uint8_t TX = 17;
+static const uint8_t RX = 18;
 
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
