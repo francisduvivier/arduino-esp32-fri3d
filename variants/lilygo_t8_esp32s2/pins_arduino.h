7e113f7cf9b51f3855ca9adb74b7af98a2a7adf2
Francis Duvivier <francisduvivier@gmail.com>
2024-08-17 14:37:19 +0200

    Fix SPI settings for LILYGO T8 ESP32S2

--- variants/lilygo_t8_esp32s2/pins_arduino.h
+++ variants/lilygo_t8_esp32s2/pins_arduino.h
@@ -25,10 +25,10 @@ static const uint8_t RX = 18;
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
 
-static const uint8_t SS    = 34;
-static const uint8_t MOSI  = 35;
-static const uint8_t MISO  = 37;
-static const uint8_t SCK   = 36;
+static const uint8_t SS    = 10;
+static const uint8_t MOSI  = 11;
+static const uint8_t MISO  = 13;
+static const uint8_t SCK   = 12;
 
 static const uint8_t A0 = 1;
 static const uint8_t A1 = 2;
